caff16586f41b0ead391839685c6e2dc11ebbf6f
Italo1033 <79849291+italo1033@users.noreply.github.com>
2024-09-05 00:36:35 -0300

    adicionando classe  containerPrincipal,  containerSideBar, containerPainel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,22 @@
-import Image from "next/image";
-import { Fragment } from "react";
+"use client"
+import { Fragment, useState } from "react";
 import Sidebar from "./components/Sidbar/sidbar";
-import CardInfor from "./components/Cardinfor/cardinfor";
 import Navbar from "./components/Navbar/navbar";
 import styles from "./page.module.css"
 
 export default function Home() {
+  const [currentComponent, setCurrentComponent] = useState(null);
   return (
       <section className={styles.containerPrincipal}>
-        <Sidebar />
-        <section>
+        <div className={styles.containerSideBar}>
+            <Sidebar />
+        </div>
+        <div className={styles.containerPainel}>
             <Navbar />
-            <div>
-              <CardInfor  valor={150} texto="teste123"/>
+            <div className={styles.ContainerComponent}>
+
             </div>
-        </section>
+        </div>
       </section>
       
   );
