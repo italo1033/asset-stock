a1ab062f39fd8e61a8a29af8eecb90eb0f67d9ac
Italo1033 <79849291+italo1033@users.noreply.github.com>
2024-09-05 00:09:40 -0300

    criando component navbar

---
+++ src/app/components/Navbar/navbar.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import styles from './navbar.module.css';
+
+const Navbar: React.FC = () => {
+    return (
+        <section className={styles.navbar}>
+            ola mundo
+        </section>
+    );
+};
+
+export default Navbar;
