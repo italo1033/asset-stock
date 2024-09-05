cbdd5a807adf0005c8f3d78dd8ff37a3fb220864
Thiago Barroso da Silva <tbsgudinho@outlook.com>
2024-09-04 21:54:21 -0300

    adicionando cardinfor na page main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 import Image from "next/image";
 import { Fragment } from "react";
 import Sidebar from "./components/Sidbar/sidbar";
+import CardInfor from "./components/Cardinfor/cardinfor";
 
 export default function Home() {
   return (
     <Fragment>
+      <div>
       <Sidebar />
+      <div>
+      <CardInfor  valor={150} texto="teste123"/>
+      </div>
+      </div>
+      
     </Fragment>
   );
 }
