63384c52986614b1b85493dfbdeba87543dae7a4
Thiago Barroso da Silva <tbsgudinho@outlook.com>
2024-09-04 21:54:42 -0300

    mudando nome da função cardinfor

--- src/app/components/Cardinfor/cardinfor.tsx
+++ src/app/components/Cardinfor/cardinfor.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import styles from "./cardinfor.module.css";
 
-export default function cardInfor({ valor, texto }: { valor: number; texto: string }) {
+export default function CardInfor({ valor, texto }: { valor: number; texto: string }) {
   return (
       <div className={styles.container}>
         <div className={styles.card}>
