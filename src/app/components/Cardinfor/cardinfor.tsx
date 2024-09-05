913e9772e796cf78ce04bc6e95e1d82d6f8bf66d
Thiago Barroso da Silva <tbsgudinho@outlook.com>
2024-09-04 21:49:16 -0300

    criando componente cardinfor

---
+++ src/app/components/Cardinfor/cardinfor.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import styles from "./cardinfor.module.css";
+
+export default function cardInfor({ valor, texto }: { valor: number; texto: string }) {
+  return (
+      <div className={styles.container}>
+        <div className={styles.card}>
+          <span className={styles.amount}>{valor}</span>
+          <span className={styles.label}>{texto}</span>
+        </div>
+      </div>
+  );
+}
