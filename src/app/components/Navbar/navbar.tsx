ae5bcd059d8f467bd7958577f10ab230e7a666f2
Italo1033 <79849291+italo1033@users.noreply.github.com>
2024-09-07 01:47:11 -0300

    adiconando icone de configuração

--- src/app/components/Navbar/navbar.tsx
+++ src/app/components/Navbar/navbar.tsx
@@ -1,10 +1,13 @@
 import { useState } from 'react';
 import styles from './navbar.module.css';
+import { IoSettings } from "react-icons/io5";
 
 const Navbar: React.FC = () => {
     return (
         <section className={styles.navbar}>
-            ola mundo
+            <div className={styles.containernavbaritens}>
+                <IoSettings size={20}/>
+            </div>
         </section>
     );
 };
