2af826445643e8de8a3e02336abe1bf0539d7e95
Italo1033 <79849291+italo1033@users.noreply.github.com>
2024-09-07 01:33:36 -0300

    ADICIONANDO onButtonClickSidebar

--- src/app/components/Sidbar/sidbar.tsx
+++ src/app/components/Sidbar/sidbar.tsx
@@ -2,38 +2,50 @@ import { useState } from 'react';
 import styles from './sidebar.module.css';
 import MenuItem from './menuItem';
 
-const Sidebar: React.FC = () => {
-    return (
-        <div className={styles.sidebar}>
-          <header>
-            Logo
-          </header>
-          
-          <menu>
-            <MenuItem title="Item 1" submenu={['Subitem 1.1', 'Subitem 1.2']} />
-            <MenuItem title="Item 2" submenu={['Subitem 2.1', 'Subitem 2.2']} />
-            <MenuItem title="Item 3" submenu={['Subitem 3.1', 'Subitem 3.2']} />
-          </menu>
+interface ButtonComponentProps {
+  onButtonClickSidebar: (item: string) => void;
+}
 
-          <footer>
+const Sidebar: React.FC<ButtonComponentProps> = ({ onButtonClickSidebar }) => {
+  const [selectedItem, setSelectedItem] = useState<string | null>(null);
+
+  const handleButtonClick = (item: string) => {
+    setSelectedItem(item);
+    if (onButtonClickSidebar) {
+      onButtonClickSidebar(item);
+    }
+  };
+
+  return (
+    <div className={styles.sidebar}>
+      <header>
+        Logo
+      </header>
+
+      <menu>
+        <MenuItem title="Item 1" submenu={['Subitem 1.1', 'Subitem 1.2']} onButtonClickMenuItem={handleButtonClick} />
+        <MenuItem title="Item 2" submenu={['Subitem 2.1', 'Subitem 2.2']} onButtonClickMenuItem={handleButtonClick} />
+        <MenuItem title="Item 3" submenu={['Subitem 3.1', 'Subitem 3.2']} onButtonClickMenuItem={handleButtonClick} />
+      </menu>
+
+      <footer>
+        <div>
+          <div>
+            <div>imagem</div>
             <div>
-              <div>
-                <div>imagem</div>
-                <div>
-                  <span>Usuario123</span>
-                  <span>Admin</span>
-                </div>
-              </div>
-
-              <div>
-                <button>Settings</button>
-                <button>logout</button>
-              </div>
+              <span>Usuario123</span>
+              <span>Admin</span>
             </div>
-          </footer>
-          
+          </div>
+
+          <div>
+            <button>Settings</button>
+            <button>Logout</button>
+          </div>
         </div>
-    );
+      </footer>
+    </div>
+  );
 };
 
 export default Sidebar;
