43216244806a8a293f9a39b850fae0d472f34a55
Italo1033 <79849291+italo1033@users.noreply.github.com>
2024-09-07 02:27:43 -0300

    adicionando componente usemenu

--- src/app/components/Navbar/navbar.tsx
+++ src/app/components/Navbar/navbar.tsx
@@ -1,15 +1,39 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import styles from './navbar.module.css';
 import { IoSettings } from "react-icons/io5";
+import UserMenu from './usermenu';
 
-const Navbar: React.FC = () => {
-    return (
-        <section className={styles.navbar}>
-            <div className={styles.containernavbaritens}>
-                <IoSettings size={20}/>
-            </div>
-        </section>
-    );
+const Navbar: React.FC<any> = () => {
+const handleSelectOption = (option: string) => {
+    switch (option) {
+        case 'settings':
+        console.log('Navigating to Settings');
+        break;
+        case 'logout':
+        console.log('Logging out');
+        break;
+        default:
+        break;
+    }
+    };
+
+
+  return (
+    <section className={styles.navbar}>
+      {/* Descomente e ajuste conforme necessário */}
+      {/* <div className={styles.containernavbaritens}>
+        <IoSettings size={20} />
+      </div> */}
+
+      
+        <UserMenu
+        logo="https://avatars.githubusercontent.com/u/79849291?v=4"
+        name="Italo Santos"
+        role="Admin"
+        onSelectOption={handleSelectOption}
+      />
+    </section>
+  );
 };
 
 export default Navbar;
