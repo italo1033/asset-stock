b790fdad544cb5ef32442b73ded7b6123cb8732c
Italo1033 <79849291+italo1033@users.noreply.github.com>
2024-09-08 10:24:20 -0300

    criando componente dashboard

---
+++ src/app/components/dashboard/dashboard.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import styles from './dashboard.module.css';
+
+const Dashboard: React.FC<any> = () => {
+
+
+  return (
+    <section className={styles.dashboard}>
+    </section>
+  );
+};
+
+export default Dashboard;
