65b66881c743961c7ed0d221757ebebd7b49b4f2
Italo1033 <79849291+italo1033@users.noreply.github.com>
2024-09-08 12:12:15 -0300

    criando layout dashboard

--- src/app/page/dashboard/dashboard.tsx
+++ src/app/page/dashboard/dashboard.tsx
@@ -6,7 +6,20 @@ const Dashboard: React.FC<any> = () => {
 
   return (
     <section className={styles.dashboard}>
-        ola mundo
+        <div className={styles.dashboardMetas}>
+
+        </div>
+
+        <div className={styles.containersubcards}>
+          <div>1</div>
+          <div>2</div>
+          <div>2</div>
+        </div>
+
+        <div className={styles.containerfinal}>
+          <div className={styles.item1}>1</div>
+          <div className={styles.item2}>2</div>
+        </div>
     </section>
   );
 };
