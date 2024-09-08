2c7b986cc295c0af926a3fc331ecc628a5b73029
Italo1033 <79849291+italo1033@users.noreply.github.com>
2024-09-08 12:11:45 -0300

    adicionando icones no aos menu de itens

--- src/app/components/Sidbar/menuItem.tsx
+++ src/app/components/Sidbar/menuItem.tsx
@@ -1,6 +1,10 @@
 "use client"
 import { useState } from 'react';
 import styles from './sidebar.module.css';
+import { TbUserHexagon } from "react-icons/tb";
+import { CgShutterstock } from "react-icons/cg";
+import { PiTicketFill } from "react-icons/pi";
+import { FaMoneyBillTransfer } from "react-icons/fa6";
 
 interface MenuItemProps {
     title: string;
@@ -15,9 +19,25 @@ const MenuItem: React.FC<MenuItemProps> = ({ title, submenu, onButtonClickMenuIt
         setIsOpen(!isOpen);
     };
 
+    const renderComponent = () => {
+        switch (title) {
+          case 'Colaboradores':
+            return <TbUserHexagon />;
+          case 'Estoque':
+            return <CgShutterstock />
+          case 'Despesas':
+            return <FaMoneyBillTransfer/>;;
+          case 'Serviços':
+            return <PiTicketFill/>;
+          default:
+            return <div></div>;
+        }
+      };
+
     return (
         <div>
             <div className={styles.menuItem} onClick={toggleSubMenu}>
+                {renderComponent()}
                 {title}
             </div>
             {isOpen && (
