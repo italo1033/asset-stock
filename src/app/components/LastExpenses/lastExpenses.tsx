5d30c50be5863080947f5b267e47de5782842cc8
Italo1033 <79849291+italo1033@users.noreply.github.com>
2024-09-08 21:12:35 -0300

    criando component lastExpenses

---
+++ src/app/components/LastExpenses/lastExpenses.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import styles from './lastExpenses.module.css';
+
+
+const LastExpenses: React.FC<any> = () => {
+
+
+  return (
+    <section className={styles.lastExpenses}>
+        
+    </section>
+  );
+};
+
+export default LastExpenses;
