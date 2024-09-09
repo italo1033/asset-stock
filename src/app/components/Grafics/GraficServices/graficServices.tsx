3f9e50e232c91afa64ccb5cb27bbc16a12c24817
Italo1033 <79849291+italo1033@users.noreply.github.com>
2024-09-08 23:26:56 -0300

    criando pasta grafic

---
+++ src/app/components/Grafics/GraficServices/graficServices.tsx
@@ -0,0 +1,90 @@
+// pages/ChartPrice.tsx
+import { useState, useEffect } from 'react';
+import dynamic from 'next/dynamic';
+import { ApexOptions } from 'apexcharts';
+
+const ReactApexChart = dynamic(() => import('react-apexcharts'), { ssr: false });
+
+const ChartServices: React.FC<any> = () => {
+  const [valorMes, setValorMes] = useState<number[]>(new Array(12).fill(0));
+
+  useEffect(() => {
+    const codUser = localStorage.getItem('codUser');
+    if (codUser) {
+      fetch(`${process.env.NEXT_PUBLIC_API_URL}/counttotalticketuser/${codUser}`)
+        .then((response) => response.json())
+        .then((result) => {
+          setValorMes(result.data);
+        })
+        .catch(console.error);
+    }
+  }, []);
+
+  const options: ApexOptions = {
+    chart: {
+      height: 290,
+      type: 'line',
+    },
+    tooltip: {
+      y: {
+        formatter: (val: number) => {
+          return `R$ ${val.toFixed(2)}`;
+        },
+      },
+    },
+    stroke: {
+      width: [2],
+    },
+    fill: {
+      colors: ['#1c74bc'],
+    },
+    title: {
+      text: 'Histórico de Gastos Mensais',
+    },
+    dataLabels: {
+      enabled: true,
+      enabledOnSeries: [0],
+    },
+    labels: [
+      'Janeiro',
+      'Fevereiro',
+      'Março',
+      'Abril',
+      'Maio',
+      'Junho',
+      'Julho',
+      'Agosto',
+      'Setembro',
+      'Outubro',
+      'Novembro',
+      'Dezembro',
+    ],
+    yaxis: [
+      {
+        title: {
+          text: 'Despesa',
+        },
+      },
+    ],
+  };
+
+  const series: ApexAxisChartSeries = [
+    {
+      name: 'Despesa',
+      type: 'column',
+      data: valorMes,
+    },
+  ];
+
+  return (
+    <ReactApexChart
+      options={options}
+      series={series}
+      type="line"
+      height={290}
+      width={'100%'}
+    />
+  );
+};
+
+export default ChartServices;
