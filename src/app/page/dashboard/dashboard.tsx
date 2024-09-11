b2a85f233c47f7a650b079fe3b3f375dfc07a4c8
Italo1033 <79849291+italo1033@users.noreply.github.com>
2024-09-11 20:08:08 -0300

    ADICIONANDO DASHBOARD

--- src/app/page/dashboard/dashboard.tsx
+++ src/app/page/dashboard/dashboard.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+// Dashboard.tsx
+import React from 'react';
 import styles from './dashboard.module.css';
 import CardInfor from '@/app/components/Cardinfor/cardinfor';
 import LastExpenses from '@/app/components/LastExpenses/lastExpenses';
@@ -6,36 +7,37 @@ import ChartServices from '@/app/components/Grafics/GraficServices/graficService
 import SalesGraphic from '@/app/components/Grafics/ChartsSales/SalesGraphic';
 import RadialBarChart from '@/app/components/Grafics/ChartUptime/chartUptime';
 
+interface DashboardProps {
+  onButtonClickDespesa: (item: string) => void;
+}
 
-const Dashboard: React.FC<any> = () => {
-
-
+const Dashboard: React.FC<DashboardProps> = ({ onButtonClickDespesa }) => {
   return (
     <section className={styles.dashboard}>
-        <div className={styles.dashboardMetas}>
-          <CardInfor texto='Ativos em uso' valor={500} />
-          <CardInfor texto='Estoque' valor={300} />
-          <CardInfor texto='Serviços' valor={30} />
-          <CardInfor texto='Serviços Abertos' valor={5} />
-          <CardInfor texto='Total de Despesas' valor={1500} />
+      <div className={styles.dashboardMetas}>
+        <CardInfor texto='Ativos em uso' valor={500} />
+        <CardInfor texto='Estoque' valor={300} />
+        <CardInfor texto='Serviços' valor={30} />
+        <CardInfor texto='Serviços Abertos' valor={5} />
+        <CardInfor texto='Total de Despesas' valor={1500} />
+      </div>
+
+      <div className={styles.containersubcards}>
+        <div>
+          <RadialBarChart />
         </div>
-
-        <div className={styles.containersubcards}>
-          <div>
-            <RadialBarChart />
-          </div>
-
-          <div>
+        <div>
           <SalesGraphic Profit={100} Expanse={100} />
-          </div>
-
-          <div>2</div>
         </div>
+        <div>2</div>
+      </div>
 
-        <div className={styles.containerfinal}>
-          <div className={styles.item1}><ChartServices/> </div>
-          <div className={styles.item2}><LastExpenses /></div>
+      <div className={styles.containerfinal}>
+        <div className={styles.item1}><ChartServices /> </div>
+        <div className={styles.item2}>
+          <LastExpenses onViewAllExpenses={() => onButtonClickDespesa('Todas Despesas')} />
         </div>
+      </div>
     </section>
   );
 };
