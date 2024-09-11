483c455b2b9e33d7e7755c8d522f4ac5a2d26f7f
Italo1033 <79849291+italo1033@users.noreply.github.com>
2024-09-11 20:07:43 -0300

    ADICIONANDO INTERFACE

--- src/app/components/LastExpenses/lastExpenses.tsx
+++ src/app/components/LastExpenses/lastExpenses.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import styles from './lastExpenses.module.css';
 import { RiMoneyDollarCircleFill } from "react-icons/ri";
+interface LastExpensesProps {
+    onViewAllExpenses: () => void; // Função de callback para mudar o componente
+  }
 
-
-const LastExpenses: React.FC<any> = () => {
+const LastExpenses: React.FC<LastExpensesProps> = ({ onViewAllExpenses }) => {
 
 
   return (
@@ -29,12 +31,12 @@ const LastExpenses: React.FC<any> = () => {
                 <div className={styles.datelhesDespesas}>
                     <RiMoneyDollarCircleFill size={40} />
                     <div className={styles.descricaodespesas}>
-                        <h5>Compra de Etiquetas</h5>
-                        <span>Variável</span>
+                        <h5>Compra de 3 notebooks</h5>
+                        <span>Compra</span>
                     </div>
                 </div>
                 <div>
-                    R$<span>2000</span>
+                    R$<span>7500</span>
                 </div>
             </div>
 
@@ -42,19 +44,19 @@ const LastExpenses: React.FC<any> = () => {
                 <div className={styles.datelhesDespesas}>
                     <RiMoneyDollarCircleFill size={40} />
                     <div className={styles.descricaodespesas}>
-                        <h5>Compra de Etiquetas</h5>
-                        <span>Variável</span>
+                        <h5>Boleto internet Vue</h5>
+                        <span>Fixa</span>
                     </div>
                 </div>
                 <div>
-                    R$<span>2000</span>
+                    R$<span>200</span>
                 </div>
             </div>
 
             
 
             <div className={styles.containerButton}>
-                <button className={styles.buttonDespesas}>
+                <button className={styles.buttonDespesas } onClick={onViewAllExpenses}>
                     Visualizar todas despesas
                 </button>
             </div>
