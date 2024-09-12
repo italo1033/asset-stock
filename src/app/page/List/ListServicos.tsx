fd56af79e058a2846b1573e04a2fb31fec4d1817
Italo1033 <79849291+italo1033@users.noreply.github.com>
2024-09-12 15:46:18 -0300

    adicionando crud

--- src/app/page/List/ListServicos.tsx
+++ src/app/page/List/ListServicos.tsx
@@ -1,19 +1,24 @@
 import styles from './list.module.css';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { FaEdit } from "react-icons/fa";
-import { RiDeleteBin5Fill } from "react-icons/ri";
-import { IoIosAddCircle } from "react-icons/io";
+import { FaEdit } from 'react-icons/fa';
+import { RiDeleteBin5Fill } from 'react-icons/ri';
+import { IoIosAddCircle } from 'react-icons/io';
+import ModalRegister from '@/app/components/modal/modal';
 
 interface Servicos {
   id: number;
   servico: string;
-  id_usuario:string;
+  id_usuario: string;
   id_Status: string;
 }
 
-const ListServicos: React.FC<any> = () => {
+const ListServicos: React.FC = () => {
   const [Servicos, setServicos] = useState<Servicos[]>([]);
+  const [showModal, setShowModal] = useState<boolean>(false);
+
+  const handleShow = () => setShowModal(true);
+  const handleClose = () => setShowModal(false);
 
   useEffect(() => {
     const fetchServicos = async () => {
@@ -28,10 +33,10 @@ const ListServicos: React.FC<any> = () => {
     fetchServicos();
   }, []);
 
-  const deleteItem = async (id:number) => {
+  const deleteItem = async (id: number) => {
     try {
-      const response = await axios.delete(`http://localhost:3001/servicos/excluir/${id}`);
-      console.log('Item excluído com sucesso:', response.data);
+      await axios.delete(`http://localhost:3001/servicos/excluir/${id}`);
+      setServicos((prevServicos) => prevServicos.filter((item) => item.id !== id)); // Atualiza a lista após exclusão
     } catch (error) {
       console.error('Erro ao excluir o item:', error);
     }
@@ -40,15 +45,17 @@ const ListServicos: React.FC<any> = () => {
   return (
     <section className={styles.containerList}>
       <div className={styles.dados}>
-        <div className={styles.add}><IoIosAddCircle size={30} /></div>
+        <div className={styles.add}>
+          <IoIosAddCircle size={30} onClick={handleShow} />
+        </div>
+
         <ul className={styles.list_dados}>
           <li style={{ width: '20%' }}>id</li>
           <li style={{ width: '20%' }}>Serviços</li>
-          <li style={{ width: '20%' }}>Responsavel</li>
+          <li style={{ width: '20%' }}>Responsável</li>
           <li style={{ width: '20%' }}>Status</li>
           <li style={{ width: '20%' }}></li>
           <li style={{ width: '20%' }}></li>
-          
         </ul>
 
         {Servicos.map((item) => (
@@ -57,11 +64,13 @@ const ListServicos: React.FC<any> = () => {
             <li style={{ width: '20%' }}>{item.servico}</li>
             <li style={{ width: '20%' }}>{item.id_usuario}</li>
             <li style={{ width: '20%' }}>{item.id_Status}</li>
-            <li style={{ width: '20%' }}><FaEdit  /></li>
-            <li style={{ width: '20%' }}><RiDeleteBin5Fill color='red' onClick={()=>deleteItem(item.id)} /></li>
+            <li style={{ width: '20%' }}><FaEdit /></li>
+            <li style={{ width: '20%' }}><RiDeleteBin5Fill color='red' onClick={() => deleteItem(item.id)} /></li>
           </ul>
         ))}
       </div>
+
+      <ModalRegister show={showModal} onHide={handleClose} componentType="Colaboradores" />
     </section>
   );
 };
